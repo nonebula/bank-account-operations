63ec264c142d6a7421781d94a63645fc43b37851
Joe Lloyd Higgs <joe@Joes-MacBook-Air.local>
2024-03-19 11:37:55 +0000

    Setup files added

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+//display greeting, account options (add, withdraw, view, transfer)
+
+// Basic Operations
+//add money
+//withdraw money
+//get current balance
+//transfer to another account
+
+// Check Minimum Balance
+//ensure funds are sufficient to withdraw (throw error if not)
+
+// Interest
+// account has a % of interest
+// get the interest amount
+    //if the interest % is 0.01 (1%) and there is 1000$, interest would be 10$
+    //do not modify savings, just know what the interest would be
+
+// Interest Ceiling
+//there is a limit on interest so the maximum to calculate on is $50,000
+//if greater than $50,000 won't be calculated above this
+
+//Favourite accounts
+//we can access the list of favorite accounts
+//we can add an account to the list
+//we can remove an account from the list by its id
