e9b643e9e59774e858911af4489a8f1ccf208a47
Joe Lloyd Higgs <joe@Joes-MacBook-Air.local>
2024-03-19 17:40:08 +0000

    Defined transfer, withdraw, interest rates and ran mock cases

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,105 @@
-//display greeting, account options (add, withdraw, view, transfer)
-
 // 1. Created a class with constructor for bank accounts
 class BankAccount {
-    private balance: number = 0;
+  private balance: number;
 
-    constructor(balance: number) {
-        this.balance = balance;
-    }
+  // 8. Interest related step
+  private interestRate: number;
+  private interestCeiling: number;
 
-    // 2. Created function to enable adding money from account
-    deposit(amount: number): void {
-        this.balance += amount;
-    }
+  // 10. Favorites creation, making a bank accounts array (default array needed otherwise will return error)
+  private favoriteAccounts: BankAccount[] = [];
+  private id;
 
-    // 3. Created function for withdrawing money from account, ensuring funds are sufficient to withdraw with if statement
-    withdraw(amount: number): void {
-        if(this.balance - amount < 0) {
-            throw new Error("Insufficient Funds")
-        } else {
-            this.balance -= amount;
-        }
-    }
+  // 9. Interest rate and ceiling added to constructor parameters to feed into new function
+  constructor(
+    id: number,
+    balance: number,
+    interestRate: number,
+    interestCeiling: number
+  ) {
+    this.id = id;
+    this.balance = balance;
+    this.interestRate = interestRate;
+    this.interestCeiling = interestCeiling;
+  }
 
-    // 4. Getter to display balance in account
-    getBalance() {
-        return this.balance;
+  // 2. Created function to enable adding money from account
+  deposit(amount: number): void {
+    this.balance += amount;
+  }
+
+  // 3. Created function for withdrawing money from account, ensuring funds are sufficient to withdraw with if statement
+  withdraw(amount: number): void {
+    if (this.balance - amount < 0) {
+      throw new Error("Insufficient Funds");
+    } else {
+      this.balance -= amount;
     }
+  }
+
+  // 4. Getter to display balance in account
+  getBalance(): number {
+    return this.balance;
+  }
+
+  // 7. Function to transfer funds. Withdraw already checking amount so no second check needed
+  transferFunds(amount: number, account: BankAccount): void {
+    this.withdraw(amount);
+    account.deposit(amount);
+  }
+
+  getInterest(): number {
+    if (this.balance > this.interestCeiling) {
+      return this.interestCeiling * this.interestRate;
+    } else {
+      return this.balance * this.interestRate;
+    }
+  }
+
+  // 11.Pushing and retrieving favorite into account array
+  addAccountToFavorites(account: BankAccount): void {
+    this.favoriteAccounts.push(account);
+  }
+
+  getFavoriteAccounts(): BankAccount[] {
+    return this.favoriteAccounts;
+  }
+
+  removeFavoriteAccountById(id: number): void {
+    const indexToRemove = this.favoriteAccounts.findIndex(
+      (account: BankAccount) => account.id === id
+    );
+    if (indexToRemove === -1) {
+      throw new Error("Account not found in favorites");
+    }
+    this.favoriteAccounts.splice(indexToRemove, 1);
+  }
 }
 
+// Provided story run-through
+
+const accountOne = new BankAccount(172849, 40000, 0.01, 50000);
+const accountTwo = new BankAccount(192848, 100000, 0.01, 50000);
+
+accountOne.addAccountToFavorites(accountTwo);
+
+accountOne.transferFunds(20000, accountOne.getFavoriteAccounts()[0]);
+
+try {
+  accountOne.withdraw(25000);
+} catch (err: unknown) {
+  console.log((err as Error).message);
+}
+
+console.log("Account Interest: ", accountOne.getInterest());
+console.log("Account Balance: ", accountOne.getBalance());
+
+accountOne.removeFavoriteAccountById(192848);
+
+//Remaining notes from process.
+
+// 5. Testing steps 1-4 and console logging
+/*
 const currentAccount = new BankAccount(5000);
 console.log("Account Balance: ", currentAccount.getBalance());
 
@@ -37,38 +109,10 @@ console.log("Account Balance: ", currentAccount.getBalance());
 currentAccount.withdraw(2000);
 console.log("Account Balance: ", currentAccount.getBalance());
 
-// currentAccount.withdraw(100000);
-// console.log("Account Balance: ", currentAccount.getBalance());
-
-// const savingsAccount = new BankAccount(50000);
-// console.log("Savings Balance: ", savingsAccount.getBalance());
-
-// savingsAccount.deposit(15000);
-// console.log("Savings Balance: ", savingsAccount.getBalance());
-
-// savingsAccount.withdraw(100);
-// console.log("Savings Balance: ", savingsAccount.getBalance());
-
-
-
-
-
-// Basic Operations
-//transfer to another account
-
-// Check Minimum Balance
-
-// Interest
-// account has a % of interest
-// get the interest amount
-    //if the interest % is 0.01 (1%) and there is 1000$, interest would be 10$
-    //do not modify savings, just know what the interest would be
-
-// Interest Ceiling
-//there is a limit on interest so the maximum to calculate on is $50,000
-//if greater than $50,000 won't be calculated above this
-
-//Favourite accounts
-//we can access the list of favorite accounts
-//we can add an account to the list
-//we can remove an account from the list by its id
+// 6. try and catch. Error type unknown by default and then forced as type 'Error'
+try{
+    currentAccount.withdraw(100000);
+} catch(err: unknown) {
+    console.log((err as Error).message);
+}
+*/
